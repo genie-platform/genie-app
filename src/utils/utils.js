c03a3c9ad552dbdc05a239ad77a0f170405d00ef
Leon Prouger <leonprou@gmail.com>
2020-05-12 15:54:27 +0300

    fixing cover not showed

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ export const getRandomCoverImage = () => {
   const NUMBER_OF_IMAGES = 10;
   const imageNumber = Math.ceil(Math.random() * NUMBER_OF_IMAGES);
 
-  return `images/cover${imageNumber}.jpg`;
+  return `/images/cover${imageNumber}.jpg`;
 };
 
 export const shortenAddress = (address) => {
