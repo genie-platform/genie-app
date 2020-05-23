ed80abae3a6e13998eb2c46f6bab29626ffb67bf
Tamir <tamir.segal1@mail.huji.ac.il>
2020-05-23 20:48:31 +0300

    mainbutton added white version

--- src/theme.js
+++ src/theme.js
@@ -10,11 +10,12 @@ export const theme = createMuiTheme({
     },
   },
   customGradients: {
-    primary: 'linear-gradient(135deg, #0089FF, #00C9E5)',
-    primaryHover: 'linear-gradient(135deg, #0089FF 0%, #00C9E5 100%)',
+    primary: 'linear-gradient(135deg, #0089FF 0%, #00C9E5 100%)',
+    primaryHover: 'linear-gradient(135deg, #4ea5f0 0%, #11cde7 100%)',
     secondary: 'linear-gradient(to right, #43e97b 0%, #38f9d7 100%)',
     error: 'linear-gradient(90deg, #cb2d3e 0%, #ef473a 100%)',
-    errorHover: 'linear-gradient(90deg, #cb2d3e, #ef473a)',
+    errorHover: 'linear-gradient(90deg, #d74454 0%, #f15f53 100%)',
+    whiteHover: 'linear-gradient(135deg, #efefef 0%, #fff 100%)',
   },
   customColors: {
     dark: '#485563',
