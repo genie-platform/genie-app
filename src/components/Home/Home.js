9792eace9414dd786f495a6d5d4f6103be04c8f9
Tamir <tamir.segal1@mail.huji.ac.il>
2020-05-23 21:57:10 +0300

    how it works component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import { connect } from 'react-redux';
@@ -9,42 +8,13 @@ import { connect } from 'react-redux';
 import IntegrateGenie from './IntegrateGenie';
 import PopularPools from './PopularPools';
 import Hero from './Hero';
+import HowItWorks from './HowItWorks';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: theme.customValues.contentWidth,
     margin: 'auto',
     textAlign: '-webkit-center',
-  },
-  heroGridItem: {
-    padding: '0',
-  },
-  hero: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundImage: theme.customGradients.primary,
-    height: '27em',
-  },
-  heroTitle: {
-    width: theme.customValues.bigTitleWidth,
-    color: 'white',
-    fontWeight: '700',
-  },
-  heroSubTitle: {
-    paddingTop: '1em',
-    width: theme.customValues.bigTitleWidth,
-    color: 'white',
-  },
-  subTitle: {
-    color: '#797979',
-    fontWeight: '600',
-    letterSpacing: '0.2em',
-    width: theme.customValues.bigTitleWidth,
-  },
-  subTitleText: {
-    width: theme.customValues.bigTitleWidth,
   },
   titleBig: {
     width: theme.customValues.bigTitleWidth,
@@ -56,13 +26,6 @@ const useStyles = makeStyles((theme) => ({
     color: 'red',
     width: '100%',
     marginBottom: '2em',
-  },
-  linkButton: {
-    color: theme.palette.primary.main,
-    fontWeight: '600',
-  },
-  link: {
-    textDecoration: 'none',
   },
   divider: {
     margin: '2em 0',
@@ -94,29 +57,7 @@ const Home = (props) => {
         <Divider className={classes.divider} />
       </Grid>
 
-      <Grid id="how-it-works" item xs={12}>
-        <Typography className={classes.subTitle} variant="subtitle2">
-          HOW IT WORKS?
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="h6" className={classes.subTitleText}>
-          Genie leverages the Ethereum Network, it uses DAI, a dollar-pegged
-          stablecoin, as a token of value, and it generates interest by lending
-          the DAI using the Compound Protocol.
-        </Typography>
-      </Grid>
-
-      <Grid item xs={12}>
-        <Button
-          className={classes.linkButton}
-          href="https://www.notion.so/Genie-9a51f51cd3044b7c87f1fe6232171f15"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </Button>
-      </Grid>
+      <HowItWorks />
     </Grid>
   );
 };
--- src/components/Home/HowItWorks.js
+++ src/components/Home/HowItWorks.js
@@ -1,1 +1,56 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  subTitle: {
+    color: '#797979',
+    fontWeight: '600',
+    letterSpacing: '0.2em',
+    width: theme.customValues.bigTitleWidth,
+  },
+  subTitleText: {
+    width: theme.customValues.bigTitleWidth,
+  },
+  linkButton: {
+    color: theme.palette.primary.main,
+    fontWeight: '600',
+  },
+}));
+
+const HowItWorks = (props) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      {' '}
+      <Grid id="how-it-works" item xs={12}>
+        <Typography className={classes.subTitle} variant="subtitle2">
+          HOW IT WORKS?
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" className={classes.subTitleText}>
+          Genie leverages the Ethereum Network, it uses DAI, a dollar-pegged
+          stablecoin, as a token of value, and it generates interest by lending
+          the DAI using the Compound Protocol.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          className={classes.linkButton}
+          href="https://www.notion.so/Genie-9a51f51cd3044b7c87f1fe6232171f15"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+export default HowItWorks;
