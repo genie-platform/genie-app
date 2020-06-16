e04b5a237ca02c81a1436d4670237b76b6b387df
Tamir <tamir.segal1@mail.huji.ac.il>
2020-06-16 18:52:12 +0300

    Failed AccountPools query

--- src/components/MyPools/MyPools.js
+++ src/components/MyPools/MyPools.js
@@ -1,12 +1,94 @@
 import React from 'react';
-import { useDepositState } from '../../services/thegraph';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { useAsync } from 'react-use';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import PoolDisplayCardWide from '../PoolDisplayCard/PoolDisplayCardWide';
+import { useAccountPoolsState } from '../../services/thegraph';
+import { fetchAllPools } from '../../ethereum/pool';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    margin: 'auto',
+    width: '50em',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+      overflow: 'hidden',
+    },
+  },
+  title: {
+    padding: '1.5em 0',
+    fontWeight: 'bold',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+}));
 
 const MyPools = (props) => {
-  const deposits = useDepositState();
+  const classes = useStyles();
+  const poolsMetadata = useAsync(async () => {
+    return fetchAllPools();
+  }, []);
 
-  console.log(deposits);
+  const accountPools = useAccountPoolsState(props.address);
+  const { loading, error } = accountPools;
+  const joinedPoolsAddresses = [];
+  let joinedPools = [];
 
-  return <div></div>;
+  if (!loading && !error && !poolsMetadata.loading) {
+    console.log('account pools', accountPools.data.accountPools);
+
+    // get user pools
+    accountPools.data.accountPools.forEach((pool) =>
+      joinedPoolsAddresses.push(pool.poolAddress)
+    );
+    console.log(joinedPoolsAddresses);
+
+    // filter only user pools metadata
+    joinedPools = poolsMetadata.value.filter(
+      (pool) => !(pool.contractAddress in joinedPoolsAddresses)
+    );
+
+    console.log(joinedPools);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h3" className={classes.title}>
+        My Joined Pools
+      </Typography>
+      <Grid container spacing={3}>
+        {joinedPools.map((pool) => (
+          <Grid item xs={12} key={pool._id}>
+            <Link
+              to={`/dashboard/${pool.contractAddress}`}
+              className={classes.link}
+            >
+              <PoolDisplayCardWide
+                name={pool.name}
+                description={pool.description}
+                icon={pool.icon}
+              />
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
-export default MyPools;
+const mapStateToProps = (state) => {
+  return {
+    address: state.auth.address,
+  };
+};
+
+export default connect(mapStateToProps, null)(MyPools);
--- src/services/thegraph.js
+++ src/services/thegraph.js
@@ -1,6 +1,16 @@
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+/**
+ * This service fetches the current state of part of our dapp that are on the blockchain, using thegraph
+ *
+ * thegraph indexes the blockchain and allows us to fetch data more easily using graphql
+ *
+ * To view the genie schema on thegraph visit https://thegraph.com/explorer/subgraph/genie-platform/genie-graph-v2
+ *
+ * for example, here we fetch the pool, rewards & deposits data.
+ */
+
 const GET_POOL = gql`
   query Pool($poolAddress: String!) {
     pool(id: $poolAddress) {
@@ -24,26 +34,67 @@ const GET_REWARDS = gql`
   }
 `;
 
+/**
+ * query to fetch a specific user's deposits
+ */
 const GET_DEPOSITS = gql`
-  query getDeposits($poolAddress: String!) {
-    deposits(where: { pool: $poolAddress }) {
+  query getDeposits($userAddress: String!) {
+    deposits(where: { sender: $userAddress }) {
       id
       txHash
       createdAt
       pool
       sender
+      amount
     }
   }
 `;
 
-export const usePoolState = async (poolAddress) =>
+const GET_ACCOUNT_POOLS = gql`
+  query getAccountPools($userAddress: String!) {
+    accountPools(where: { account: $userAddress }) {
+      id
+      poolAddress
+      balance
+      account {
+        address
+      }
+    }
+  }
+`;
+
+/**
+ * return the pool state
+ * @param {string} poolAddress the pool's smart contract address
+ */
+export const usePoolState = (poolAddress) =>
   useQuery(GET_POOL, {
     variables: { poolAddress },
   });
 
-export const useRewardsState = async (poolAddress) =>
+/**
+ * return the reward state
+ * @param {string} poolAddress the pool's smart contract address
+ */
+export const useRewardsState = (poolAddress) =>
   useQuery(GET_REWARDS, {
     variables: { poolAddress },
   });
 
-export const useDepositState = async (userAddress) => useQuery(GET_DEPOSITS);
+/**
+ * returns all deposits of a certain user address
+ * @param {string} userAddress the user address
+ */
+export const useDepositState = (userAddress) =>
+  useQuery(GET_DEPOSITS, {
+    variables: { userAddress },
+  });
+
+/**
+ * returns the pools the user is in
+ * @param {string} userAddress the user address
+ */
+export const useAccountPoolsState = (userAddress) =>
+  useQuery(GET_ACCOUNT_POOLS, {
+    variables: { userAddress },
+  });
